bdd606ae64c2b6e24ee2c09cb05b2c6b8fbbb3b0
OOHYOJJOO <dps10y@naver.com>
2018-04-19 01:25:07 +0900

    commit

---
+++ User.java
@@ -0,0 +1,44 @@
+package com.company.train;
+
+public class User {
+	private String userId;
+	private String password;
+	private String userName;
+	private String userPno;
+	
+	/*ID정보받기*/
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	/*PW정보받기*/
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	/*Name정보받기*/
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	/*Pno정보받기*/
+	public String getUserPno() {
+		return userPno;
+	}
+	public void setUserPno(String userPno) {
+		this.userPno = userPno;
+	}
+	
+	
+	
+	
+}
